6d8d24fd90e031c88711560310f4469732824bb8
MarsGotBars <marcinm@live.nl>
2024-09-14 01:07:35 +0200

    temp
    added useful link

--- scripts/m-script.js
+++ scripts/m-script.js
@@ -4,3 +4,6 @@ console.log(sectioning[0].children[0].classList);
 window.onscroll=(e)=>{
     console.log(window.scrollY);
 }
+
+// https://savvy.co.il/en/blog/wordpress-design/css-scroll-snapping/
+// intersection api examples
