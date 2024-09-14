18dc8a2591f5cfc41515770e68761ac74ef8efbe
Marcin Magdziak <marcinm@live.nl>
2024-09-14 12:34:22 +0200

    updated

--- scripts/m-script.js
+++ scripts/m-script.js
@@ -1,9 +1,9 @@
-const sectioning = document.querySelectorAll('section');
+const sectioning = document.querySelectorAll("section");
 console.log(sectioning[0].children[0].classList);
 
-window.onscroll=(e)=>{
-    console.log(window.scrollY);
-}
+window.onscroll = (e) => {
+  console.log(window.scrollY);
+};
 
 // https://savvy.co.il/en/blog/wordpress-design/css-scroll-snapping/
 // intersection api examples
